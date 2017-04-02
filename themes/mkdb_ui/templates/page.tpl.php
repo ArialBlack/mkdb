50f7854a3bc29b06a6d4e90144db98788d00e0c9
ArialBlack <andreyturik@gmail.com>
2017-04-02 20:29:26 +0300

    fixes

--- themes/mkdb_ui/templates/page.tpl.php
+++ themes/mkdb_ui/templates/page.tpl.php
@@ -145,12 +145,34 @@
   <div class="content-bottom"><?php print render($page['content_bottom']); ?></div>
 <?php endif; ?>
 
-<?php if (!empty($page['footer'])): ?>
-    <footer class="footer">
-        <div class="container">
-            <?php print render($page['footer']); ?>
+<div class="footer-container">
+    <header role="banner" class="navbar container navbar-inverse">
+        <div class="<?php print $container_class; ?>">
+            <div class="navbar-header">
+                <?php if ($logo): ?>
+                    <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+                        <?php include("/sites/all/themes/mkdb_ui/images/logo.svg"); ?>
+                    </a>
+                <?php endif; ?>
+
+                <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+                <?php endif; ?>
+            </div>
+
+            <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+                <div class="navbar-collapse collapse">
+                    <nav role="navigation">
+                        <?php if (!empty($page['navigation'])): ?>
+                            <?php print render($page['navigation']); ?>
+                        <?php endif; ?>
+                        <?php if (!empty($primary_nav)): ?>
+                            <?php print render($primary_nav); ?>
+                        <?php endif; ?>
+                    </nav>
+                </div>
+            <?php endif; ?>
         </div>
-    </footer>
-<?php endif; ?>
+    </header>
+</div>
 
 
