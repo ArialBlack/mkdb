905dc972a1b12ffeb4b6d2cd0709e8fd5d991256
ArialBlack <andreyturik@gmail.com>
2017-04-05 16:12:13 +0300

    deep styling

--- themes/mkdb_ui/_templates-src/parts/footer.php
+++ themes/mkdb_ui/_templates-src/parts/footer.php
@@ -13,11 +13,8 @@
             </div>
 
             <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-                <div class="navbar-collapse collapse">
+                <div class="">
                     <nav role="navigation">
-                        <?php if (!empty($page['navigation'])): ?>
-                            <?php print render($page['navigation']); ?>
-                        <?php endif; ?>
                         <?php if (!empty($primary_nav)): ?>
                             <?php print render($primary_nav); ?>
                         <?php endif; ?>
